619c8fa743643efa4c3bceca8884ca20b3002846
charithayadati <charithay522@gmail.com>
2019-07-17 17:27:45 +0530

    first commit

---
+++ src/main/java/com/stackroute/User.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class User {
+
+    public String giveName(){
+
+        return "Charitha";
+    }
+}
---
+++ src/main/java/com/stackroute/UserController.java
@@ -0,0 +1,18 @@
+package com.stackroute;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class UserController {
+  @Autowired
+  public User user;
+  @RequestMapping("/")
+    public String show(Model model) {
+        model.addAttribute("name",user.giveName());
+        return "index";
+    }
+}
